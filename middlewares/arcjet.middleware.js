c53c24219dd29803850a14863ee23b5bdd47378a
williams bolu <williams.bolu99@gmail.com>
2025-05-23 23:42:59 +0100

    concluded testing

--- middlewares/arcjet.middleware.js
+++ middlewares/arcjet.middleware.js
@@ -1,22 +1,22 @@
 import aj from "../config/arcjet.js";
 
 const arcjetMiddleware = async (req, res, next) => {
-  console.log("Client IP:", req.ip);
-  console.log("X-Forwarded-For:", req.get("X-Forwarded-For"));
-  console.log("Request IP:", req.ip);
-  console.log("User-Agent:", req.headers["user-agent"]);
-  console.log("Accept-Language:", req.headers["accept-language"]);
+  // console.log("Client IP:", req.ip);
+  // console.log("X-Forwarded-For:", req.get("X-Forwarded-For"));
+  // console.log("Request IP:", req.ip);
+  // console.log("User-Agent:", req.headers["user-agent"]);
+  // console.log("Accept-Language:", req.headers["accept-language"]);
 
-  // ? NOT IMPORTANT, CODE STILL WORKS PERFECTLY: JUST PREVENT ARCJET FROM RUNNING ON INITIAL BUILD AND DURING HEALTH CHECKS ON RENDER PLATFORM.
+  // ? NOT IMPORTANT, CODE STILL WORKS PERFECTLY: JUST PREVENT ARCJET FROM RUNNING ON INITIAL BUILD DISPLAYING SOME IP ERRORS AND DURING HEALTH CHECKS ON RENDER PLATFORM BECAUSE ARCJET REQUIRES THE IP OF THE REQUEST FROM THE CLIENT AND THOSE RENDER internal REQUEST DOSENT PROVIDE THAT.
   // Skip Arcjet for health checks by render on initial build (no X-Forwarded-For or localhost IP)
-  if (
-    !req.get("X-Forwarded-For") ||
-    req.ip === "::1" ||
-    req.ip === "127.0.0.1"
-  ) {
-    console.log("Skipping Arcjet for health check request");
-    return next();
-  }
+  // if (
+  //   !req.get("X-Forwarded-For") ||
+  //   req.ip === "::1" ||
+  //   req.ip === "127.0.0.1"
+  // ) {
+  //   console.log("Skipping Arcjet for health check request");
+  //   return next();
+  // }
 
   try {
     const decision = await aj.protect(req, { requested: 1 });
