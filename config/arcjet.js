aa1c32655a6d02e4d29cb7ba1ecc03e5f98c818a
williams bolu <williams.bolu99@gmail.com>
2025-05-23 16:28:07 +0100

    rate limi testing

--- config/arcjet.js
+++ config/arcjet.js
@@ -17,8 +17,8 @@ const aj = arcjet({
     tokenBucket({
       mode: "LIVE",
       refillRate: 5, // Refill 5 tokens per interval
-      interval: 10, // Refill every 10 seconds
-      capacity: 10, // Bucket capacity of 10 tokens
+      interval: 60, // Refill every 10 seconds
+      capacity: 5, // Bucket capacity of 10 tokens
     }),
   ],
 });
