fa20540f7971a98f7b0a40bfddbed5f28dcf1909
williams bolu <williams.bolu99@gmail.com>
2025-05-23 12:28:20 +0100

    updated proxy settings

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ import workflowRouter from "./routes/workflow.routes.js";
 
 const app = express();
 
+app.set("trust proxy", true);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
